4968b0aa87d34f54dfe0e14d4abaafdb7054f54c
Angelrp11 <angelrp.mail@gmail.com>
2022-02-17 18:34:54 +0100

    aa

--- resources/js/peticion1.js
+++ resources/js/peticion1.js
@@ -10,7 +10,7 @@ function cambiaSelectHoras() {
     let horas = ['08:00:00','09:30:00','11:00:00','12:30:00','14:00:00','15:30:00','17:00:00','18:30:00','20:00:00','22:30:00'];
     let selectHoras = document.getElementById('hora');
     let option ='';
-    $.get('/Laravel/proyectoLaravel/public/api/dia/'+dia+'/pista/'+pista+'/horas', function(data) {
+    $.get('./api/dia/'+dia+'/pista/'+pista+'/horas', function(data) {
         function removeItemFromArr ( arr, item ) {
             var i = arr.indexOf(item);
             arr.splice( i, 1 );
